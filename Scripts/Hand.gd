ec50e5beccc78414af3a9d3708402185b0aab70f
EthanDenny <ethandenny@protonmail.com>
2022-12-15 13:49:22 -0330

    Fixed slowdowns caused by object collisions
    The old system relied on deleting and creating new objects - the new system simply updates all the properties of the eatee  to be the same as the eater.

--- Scripts/Hand.gd
+++ Scripts/Hand.gd
@@ -5,23 +5,20 @@ export(int) var type_id
 export var prey_ids = []
 export var predator_ids = []
 
-var speed = 2
-var velocity = Vector2()
-
+var speed = 0
 var target = null
-var initial_target_distance = 0
+
+
+func _ready():
+	find_target()
 
 
 func _physics_process(delta):
-	if target == null:
-		target = find_target()
-	elif not is_instance_valid(target):
-		target = find_target()
-	elif position.distance_to(target.position) > initial_target_distance * 2:
-		target = find_target()
+	if target != null and target.type_id == type_id:
+			find_target()
 	
 	if target != null:
-		velocity = position.direction_to(target.position) * speed
+		var velocity = position.direction_to(target.position) * speed
 		position += velocity
 	
 	for body in $Area2D.get_overlapping_areas():
@@ -34,14 +31,13 @@ func _physics_process(delta):
 
 
 func eat(hand):
-	var new_hand = self.duplicate()
-	new_hand.position = hand.position
-	hand.queue_free()
-	get_parent().add_child(new_hand)
+	hand.find_node("Sprite").texture = $Sprite.texture
+	hand.type_id = type_id
+	hand.prey_ids = prey_ids
+	hand.predator_ids = predator_ids
+	find_target()
+	hand.find_target()
 
 
 func find_target():
 	target = get_parent().nearest(self, prey_ids)
-	if target != null:
-		initial_target_distance = position.distance_to(target.position)
-	return target
