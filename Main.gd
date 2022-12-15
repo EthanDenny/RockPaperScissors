88dbf41799d05e712b358fff7e74d777e6c5a143
EthanDenny <ethandenny@protonmail.com>
2022-12-15 12:25:12 -0330

    Created nearest() function
    nearest() takes a position and an array of hand ids, then either returns the nearest hand with one of the supplied ids, or no hand with such an id can be found, the nearest hand

--- Main.gd
+++ Main.gd
@@ -15,3 +15,23 @@ func _process(delta):
 		var new_hand = load(hands[0]).instance()
 		new_hand.position = get_global_mouse_position()
 		add_child(new_hand)
+
+
+func nearest(pos : Vector2, ids : Array):
+	var nearest_no_type
+	var nearest_with_type = null
+	
+	for child in get_children():
+		var dist = pos.distance_to(child.position)
+		var near_dist = pos.distance_to(nearest_no_type.position)
+		var near_type_dist = pos.distance_to(nearest_with_type.position)
+		
+		if dist < near_dist:
+			nearest_no_type = child
+		if dist < nearest_with_type and child.type_id in ids:
+			nearest_with_type = child
+	
+	if nearest_with_type == null:
+		return nearest_no_type
+	else:
+		return nearest_with_type
