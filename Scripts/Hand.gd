7dbe9ed8a69b108d42289666023e55da3acabd80
EthanDenny <ethandenny@protonmail.com>
2022-12-15 13:07:47 -0330

    Rewrote hand movement

--- Scripts/Hand.gd
+++ Scripts/Hand.gd
@@ -5,21 +5,26 @@ export(int) var type_id
 export var prey_ids = []
 export var predator_ids = []
 
+var speed = 2
 var velocity = Vector2()
 
-
-func _ready():
-	velocity = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized() * 10
+var target = null
+var initial_target_distance = 0
 
 
 func _physics_process(delta):
-	position += velocity
+	if target == null:
+		target = find_target()
+	elif not is_instance_valid(target):
+		target = find_target()
+	elif position.distance_to(target.position) > initial_target_distance * 2:
+		target = find_target()
+	
+	if target != null:
+		velocity = position.direction_to(target.position) * speed
+		position += velocity
 	
 	for body in $Area2D.get_overlapping_areas():
-		var mag = velocity.length()
-		var dir = velocity.normalized() * rand_range(-1, 1)
-		velocity = dir * mag
-		
 		var hand = body.get_parent()
 		if hand.type_id != type_id:
 			if hand.type_id in prey_ids:
@@ -31,6 +36,12 @@ func _physics_process(delta):
 func eat(hand):
 	var new_hand = self.duplicate()
 	new_hand.position = hand.position
-	new_hand.velocity = hand.velocity
 	hand.queue_free()
 	get_parent().add_child(new_hand)
+
+
+func find_target():
+	target = get_parent().nearest(self, prey_ids)
+	if target != null:
+		initial_target_distance = position.distance_to(target.position)
+	return target
